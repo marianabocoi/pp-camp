a11e0d47c39fd0ee39c2f18cc64491b5740bd079
Mariana <mariana.bocoi@gmail.com>
2017-07-12 04:36:45 +0200

    Graphs work - comparison sw - spain

--- d4/population.py
+++ d4/population.py
@@ -3,9 +3,9 @@ from pprint import pprint
 import matplotlib.pyplot as plt
 from matplotlib.ticker import ScalarFormatter
 
-r = requests.get('http://api.worldbank.org/countries/swe/indicators/SP.POP.TOTL?format=json&per_page=100')
+r = requests.get('http://api.worldbank.org/countries/swe;esp/indicators/SP.POP.TOTL?format=json&per_page=1000')
 data = r.json()
-#pprint(data)
+# pprint(data)
 
 pop = data[1]
 
@@ -17,22 +17,45 @@ for entry in pop:
     else:
         pop_per_country[country_id] = {}
 
-#pprint(pop_per_country)
+# pprint(pop_per_country)
 
-x=[]
-y=[]
+x_se = []
+y_se = []
 for year, value in pop_per_country['SE'].items():
-    x.append(int(year))
-    y.append(int(value))
+    x_se.append(int(year))
+    y_se.append(int(value))
 
-pprint(x)
-pprint(y)
-
+x_es = []
+y_es = []
+for year, value in pop_per_country['ES'].items():
+    x_es.append(int(year))
+    y_es.append(int(value))
+pprint(x_es)
+pprint(x_se)
 fig, ax = plt.subplots()
 
-ax.fill_between(x, 7484656, y)
-ax.grid(True, zorder=5)
-ax.set_ylabel('Total population SE')
+# line-plot
+
+fig, ax = plt.subplots()
+line1, = ax.plot(x_se, y_se, '--', color='teal', linewidth=2,
+                 label='Sweden Popiulation yearly growth')
+
+line2, = ax.plot(x_es, y_es, ',:', color='orange', linewidth=2,
+                 label='Spain Popiulation yearly growth')
+
+ax.legend(loc='best')
 ax.set_xlabel('Year')
-ax.set_title('')
+ax.set_ylabel('Total population')
+ax.set_title('Comparison')
+
 plt.show()
+
+
+
+# fill-plot
+# ax.fill_between(x, 7484656, y)
+# ax.grid(True, zorder=5)
+# ax.set_ylabel('Total population SE')
+# ax.set_xlabel('Year')
+# ax.set_title('')
+# plt.show()
